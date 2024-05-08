f9229045c7867a2a348c60de36d2dcc3128457c4
JavierMtzRdz <javierwolfsangel@gmail.com>
2024-05-07 17:48:46 -0700

    Adding theme

--- code/appear.sh
+++ code/appear.sh
@@ -8,5 +8,20 @@ wget https://github.com/amirmasoudabdol/rstudio-icons/raw/main/Icons/macOS/Recta
 
 touch /Applications/RStudio.app
 
+# Install firacode
+
+#brew tap homebrew/cask-fonts
+#brew install --cask font-fira-code
+
+# Update theme
+
+wget https://raw.githubusercontent.com/JavierMtzRdz/appear-RStudio/main/frmt/base16-JMR.rstheme -O ~/.config/rstudio/themes/base16-JMR.rstheme
+
+R -e 'if (!require("jsonlite")) install.packages("jsonlite")
+location <- "~/.config/rstudio/rstudio-prefs.json"
+json_list <- jsonlite::read_json(location)
+json_list$editor_theme <- "base16 JMR {rsthemes}"
+jsonlite::write_json(json_list, location, pretty=TRUE, auto_unbox = TRUE)
+'
 
 echo Done!
