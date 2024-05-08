b03f765199faedbea3e81e3a6c808249e201d317
JavierMtzRdz <javierwolfsangel@gmail.com>
2024-05-07 23:37:24 -0700

    Adding curl version

--- code/appear.sh
+++ code/appear.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+
 
 # Update icon
-
-wget https://github.com/amirmasoudabdol/rstudio-icons/raw/main/Icons/macOS/Rectangle/Dark%20Icon%20-%20Rect.icns -O /Applications/RStudio.app/Contents/Resources/RProject.icns
+curl -L -o /Applications/RStudio.app/Contents/Resources/RProject.icns https://github.com/amirmasoudabdol/rstudio-icons/raw/main/Icons/macOS/Rectangle/Dark%20Icon%20-%20Rect.icns
+#wget https://github.com/amirmasoudabdol/rstudio-icons/raw/main/Icons/macOS/Rectangle/Dark%20Icon%20-%20Rect.icns -O /Applications/RStudio.app/Contents/Resources/RProject.icns
 
 wget https://github.com/amirmasoudabdol/rstudio-icons/raw/main/Icons/macOS/Rectangle/Dark%20Icon%20-%20Rect.icns -O /Applications/RStudio.app/Contents/Resources/RStudio.icns
 
